f00cc0e7d67358bd54aebda4a3a9cc387beeb14c
syxxne <tjdus562@naver.com>
2023-11-20 13:44:35 +0900

    first commit

---
+++ src/main/java/com/careerup/careerupspring/CareerUpSpringApplication.java
@@ -0,0 +1,13 @@
+package com.careerup.careerupspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerUpSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerUpSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerup/careerupspring/CareerUpSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerup.careerupspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerUpSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
