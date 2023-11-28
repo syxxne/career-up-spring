db301618a3264d9098e37b80101fb1d064c36fcb
jekyoung <best970611@naver.com>
2023-11-28 09:20:55 +0900

    CORS 오류 코드 추가

--- src/main/java/com/careerup/careerupspring/CareerUpSpringApplication.java
+++ src/main/java/com/careerup/careerupspring/CareerUpSpringApplication.java
@@ -2,12 +2,28 @@ package com.careerup.careerupspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CareerUpSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CareerUpSpringApplication.class, args);
+	}
+
+	@Bean // WebMvcConfigurer 인터페이스를 구현한 객체를 빈으로 등록
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // 특정 엔드포인트에 대한 CORS 설정
+						.allowedOrigins("http://localhost:3000") // 프론트엔드 서버 주소
+						.allowedHeaders("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH"); // 허용할 HTTP 메서드
+			}
+		};
 	}
 
 }
